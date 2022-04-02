8c492f7dfe4eb21ce969beefab9d80273ce8a09f
CatMe0w <CatMe0w@live.cn>
2022-04-03 01:28:01 +0800

    Add OmniaThread

---
+++ src/OmniaThread.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+  export let page: number;
+  export let time: string | null;
+  export let searchKeyword: string | null;
+
+  const getThreads = async () => {
+    let url = "https://catme0w.org/ex_nihilo_vault/thread/" + page;
+    let params = new URLSearchParams();
+    if (time) params.set("time_machine_datetime", time);
+    if (searchKeyword) params.set("search_keyword", searchKeyword);
+    let response = await fetch(url + "?" + params.toString());
+    let json = await response.json();
+    return json;
+  };
+
+  const renderOpPost = (json: any[]) => {
+    let opPost = "";
+    json.forEach((item) => {
+      if (item.type === "text") {
+        opPost += item.content;
+        opPost += " ";
+      }
+    });
+    return opPost;
+  };
+
+  const getThreadLink = (threadId: string) => {
+    const currentUrl = new URL(location.href);
+    const params = new URLSearchParams(currentUrl.search);
+    params.set("t", threadId);
+    params.delete("p");
+    return new URL(
+      currentUrl.origin + currentUrl.pathname + "?" + params.toString()
+    ).toString();
+  };
+
+  // todo: pagnation, inner header, photo preview, video preview, is_good tags
+</script>
+
+<div class="bg-gray-50 mx-1 my-2">
+  <div class="shadow rounded bg-white">
+    {#await getThreads()}
+      <p class="p-6">刷刷刷……</p>
+    {:then json}
+      <div class="grid grid-cols-1">
+        {#each json.threads as thread, i}
+          <div class="p-6 border-b border-gray-100">
+            <div class="flex flex-row">
+              <div class="mr-4 basis-14 shrink-0">
+                <p class="px-2 py-1.5 bg-gray-100 rounded text-sm text-center">
+                  {thread.reply_num}
+                </p>
+              </div>
+              <div class="truncate grow">
+                <a
+                  href={getThreadLink(thread.thread_id)}
+                  class="text-sky-700">{thread.title}</a
+                >
+                <p class="text-sm mt-2 truncate text-gray-700">
+                  {renderOpPost(thread.op_post_content)}
+                </p>
+              </div>
+              <div
+                class="justify-self-end basis-28 shrink-0 text-xs pl-3 truncate text-gray-500"
+              >
+                <p class="mb-1.5 truncate">
+                  <span class="grayscale">👤</span>
+                  {json.op_users[i].nickname}
+                </p>
+                <p class="mb-1.5 truncate">
+                  <span class="grayscale">💬</span>
+                  {json.last_reply_users[i].nickname}
+                </p>
+                <p>
+                  <span class="grayscale">🕒</span>
+                  {thread.time.slice(5, -3)}
+                </p>
+              </div>
+            </div>
+          </div>
+        {/each}
+      </div>
+    {/await}
+  </div>
+</div>
