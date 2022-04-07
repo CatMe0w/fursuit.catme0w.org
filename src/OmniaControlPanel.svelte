771ecd4edeab5eb4daac9d3261265ff79f4fe2e3
CatMe0w <CatMe0w@live.cn>
2022-04-07 15:42:49 +0800

    Fix broken layout on Apple IE6 (aka Safari)

--- src/OmniaControlPanel.svelte
+++ src/OmniaControlPanel.svelte
@@ -18,17 +18,17 @@
       <p class="mt-5 mb-2 pt-5 border-t">当前所在时间</p>
       <p class="font-sevenseg text-2xl text-gray-700">2016-07-08<br />00:42:42</p>
       <p class="mt-5 mb-2 pt-5 border-t">前往下一个时间</p>
-      <div class="flex flex-col justify-center">
+      <div class="flex flex-col items-center">
         <input
           id="date"
           type="date"
           value="2011-03-19"
           min="2011-03-19"
           max="2022-02-16"
-          class="font-sevenseg text-gray-600 mb-2 text-xl border-2 border-gray-200 rounded text-center"
+          class="font-sevenseg text-gray-600 mb-2 text-xl w-full border-2 border-gray-200 rounded text-center"
         />
-        <input id="time" type="time" step="1" value="04:42" class="font-sevenseg text-gray-600 text-xl border-2 border-gray-200 rounded text-center" />
-        <button class="mt-2 bg-blue-600 text-white font-bold py-1.5 rounded">发射</button>
+        <input id="time" type="time" step="1" value="04:42" class="font-sevenseg text-gray-600 text-xl w-full border-2 border-gray-200 rounded text-center" />
+        <button class="mt-3 bg-blue-600 text-white font-bold px-8 py-1.5 rounded">发射</button>
       </div>
     </div>
   </div>
