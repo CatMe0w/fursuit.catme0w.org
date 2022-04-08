40e487686f146c2070fadf9feba39d72600e51c7
CatMe0w <CatMe0w@live.cn>
2022-04-08 15:09:47 +0800

    Add backToThreads

--- src/OmniaHeader.svelte
+++ src/OmniaHeader.svelte
@@ -12,12 +12,21 @@
     subtitle = "一切存在过的帖子与回复。";
   }
 
+  const backToThreads = () => {
+    const currentUrl = new URL(location.href);
+    const currentParams = new URLSearchParams(currentUrl.search);
+    const time = currentParams.get("time");
+    const newParams = new URLSearchParams();
+    if (time) newParams.set("time", time);
+    return currentUrl.origin + currentUrl.pathname + "?" + newParams.toString();
+  };
+
   // todo: back button, and...?
 </script>
 
 <header class="p-6 mt-12">
   <div class="mb-12">
-    <h1 class="text-3xl mb-3">{title}</h1>
+    <h1 class="text-3xl mb-3"><a href={backToThreads()}>{title}</a></h1>
     <p>{subtitle}</p>
   </div>
 </header>
