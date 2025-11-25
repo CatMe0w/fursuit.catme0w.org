f0d1916e8b2cd9009d4e2edaed45871ee7e6c1b0
CatMe0w <CatMe0w@live.cn>
2025-11-24 23:20:55 -0700

    Fix 404 pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import svelte from '@astrojs/svelte';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fursuit.catme0w.org',
+  site: "https://fursuit.catme0w.org",
   integrations: [sitemap(), svelte()],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  build: {
+    format: "file",
+  },
 });
