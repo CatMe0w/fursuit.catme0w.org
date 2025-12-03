f2709702db7597c65a1611a5584ba743c00e41cb
CatMe0w <CatMe0w@live.cn>
2025-12-02 17:46:44 -0700

    Fix SSG Lightbox by moving out of svelte

--- src/components/ContentImage.svelte
+++ src/components/ContentImage.svelte
@@ -9,16 +9,14 @@
 
   let width = $state(0);
   let height = $state(0);
-  let loaded = $state(false);
 
   function onLoad(e: Event) {
     const img = e.target as HTMLImageElement;
     width = img.naturalWidth;
     height = img.naturalHeight;
-    loaded = true;
   }
 </script>
 
-<a href={src} data-pswp-width={width} data-pswp-height={height} target="_blank" class:gallery-item={loaded}>
+<a href={src} data-pswp-width={width} data-pswp-height={height} target="_blank" class="gallery-item">
   <img class={variant === "album" ? "w-full mb-2 block rounded" : "w-auto lg:max-w-xl my-2 inline"} {src} {alt} onload={onLoad} />
 </a>
